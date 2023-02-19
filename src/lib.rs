edd5bb1a95434d74ace0d4074c6c13c4057dc485
SamokhinIlya <samokhin.ilya.97@gmail.com>
2023-02-19 21:31:20 +0300

    improve performance 2x

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ pub fn mandelbrot(c: Complex64, precision: usize) -> usize {
     successors(Some(c), |z| Some(z*z + c))
         .enumerate()
         .take(precision)
-        .find(|(_, z)| z.norm() > 2.0)
+        .find(|(_, z)| z.norm_sqr() > 4.0)
         .map(|(i, _)| i)
         .unwrap_or(precision)
 }
