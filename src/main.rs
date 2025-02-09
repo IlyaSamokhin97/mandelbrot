3aa5c6b6881f51c4a513f2b1e49b58a388d45a7e
Ilya Samokhin <Ilya.Samokhin.97@yandex.ru>
2025-02-09 13:09:31 +0300

    simplify a bit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::{iter::successors};
+use std::iter::successors;
+use std::num::Saturating;
 
+use itertools::Itertools;
 use num::complex::Complex64;
 use rayon::prelude::*;
 
@@ -17,14 +19,13 @@ impl lume::Data for Data {
     fn update(&mut self, raw_canvas: &mut dyn lume::RawCanvas, input: &lume::Input, dt: f64) {
         let precision = 100;
 
-        if let Some(mov) = move_vector(input) {
-            self.center += mov * dt;
-        }
+        self.center += move_vector(input) * dt;
 
         let height = raw_canvas.height();
         let width = raw_canvas.width();
 
-        let changes: Vec<_> = (0..height).into_par_iter().flat_map(|y| (0..width).into_par_iter().map(move |x| (y, x)))
+        let changes: Vec<_> = (0..height).cartesian_product(0..width).collect_vec()
+            .into_par_iter()
             .map(|(y, x)| {
                 let c = screen_to_complex((y, x), width, self);
 
@@ -37,56 +38,57 @@ impl lume::Data for Data {
             raw_canvas[i] = c;
         }
 
-        let (y, x) = (
-            input.mouse.y,
-            input.mouse.x,
-        );
-        let len: usize = 10;
+        let [y, x] = [input.mouse.y, input.mouse.x].map(Saturating);
+        let radius = Saturating(5);
 
-        line(raw_canvas, [[y                      , x.saturating_sub(len/2)], [y        , x + len/2]]);
-        line(raw_canvas, [[y.saturating_sub(len/2), x                      ], [y + len/2, x        ]]);
+        line(raw_canvas, [[y         , x - radius], [y         , x + radius]]);
+        line(raw_canvas, [[y - radius, x         ], [y + radius, x         ]]);
     }
 }
 
-fn line(canvas: &mut dyn lume::RawCanvas, ps: [[usize; 2]; 2]) {
-    let ps = if ps[0][1] > ps[1][1] {
-        [ps[1], ps[0]]
-    } else {
-        ps
-    };
+fn line(canvas: &mut dyn lume::RawCanvas, ps: [[Saturating<usize>; 2]; 2]) {
+    fn line_impl(canvas: &mut dyn lume::RawCanvas, ps: [[usize; 2]; 2]) {
+        let ps = if ps[0][1] > ps[1][1] {
+            [ps[1], ps[0]]
+        } else {
+            ps
+        };
 
-    let [[y0,x0], [y1,x1]] = ps;
+        let [[y0,x0], [y1,x1]] = ps;
 
-    let height = canvas.height();
-    let width = canvas.width();
-    if x0 != x1 {
-        // y = (y1 - y0)/(x1 - x0) * x + y0
+        let height = canvas.height();
+        let width = canvas.width();
+        if x0 != x1 {
+            // y = (y1 - y0)/(x1 - x0) * x + y0
 
-        let slope = (y1 - y0) / (x1 - x0);
-        for x in x0..=x1 {
-            let y = slope * x + y0;
-            if x < width && y < height {
-                let px = &mut canvas[y*width + x];
-                if *px == 0xFFFF_FFFF {
-                    *px = 0xFF00_0000;
-                } else {
-                    *px = 0xFFFF_FFFF;
+            let slope = (y1 - y0) / (x1 - x0);
+            for x in x0..=x1 {
+                let y = slope * x + y0;
+                if x < width && y < height {
+                    let px = &mut canvas[y*width + x];
+                    if *px == 0xFFFF_FFFF {
+                        *px = 0xFF00_0000;
+                    } else {
+                        *px = 0xFFFF_FFFF;
+                    }
                 }
             }
-        }
-    } else {
-        let x = x0;
-        for y in y0..=y1 {
-            if x < width && y < height {
-                let px = &mut canvas[y*width + x];
-                if *px == 0xFFFF_FFFF {
-                    *px = 0xFF00_0000;
-                } else {
-                    *px = 0xFFFF_FFFF;
+        } else {
+            let x = x0;
+            for y in y0..=y1 {
+                if x < width && y < height {
+                    let px = &mut canvas[y*width + x];
+                    if *px == 0xFFFF_FFFF {
+                        *px = 0xFF00_0000;
+                    } else {
+                        *px = 0xFFFF_FFFF;
+                    }
                 }
             }
         }
     }
+
+    line_impl(canvas, ps.map(|xs| xs.map(|x| x.0)))
 }
 
 fn screen_to_complex((y, x): (usize, usize), width: usize, data: &Data) -> Complex64 {
@@ -101,18 +103,18 @@ fn range_map(n: f64, [sb, se]: [f64; 2], [db, de]: [f64; 2]) -> f64 {
     (n - sb) * scale + db
 }
 
-fn move_vector(input: &lume::Input) -> Option<Complex64> {
+fn move_vector(input: &lume::Input) -> Complex64 {
     let kb = &input.keyboard;
 
     [
         (&kb.left , (-1.0,  0.0)),
         (&kb.right, ( 1.0,  0.0)),
-        (&kb.up   , ( 0.0,  1.0)),
-        (&kb.down , ( 0.0, -1.0)),
+        (&kb.up   , ( 0.0, -1.0)),
+        (&kb.down , ( 0.0,  1.0)),
     ]
         .into_iter()
         .filter_map(|(btn, (re, im))| btn.is_pressed().then(|| Complex64::new(re, im)))
-        .fold(None, |acc, x| Some(x + acc.unwrap_or_else(num::zero)))
+        .sum()
 }
 
 fn mandelbrot(c: Complex64, precision: usize) -> usize {
